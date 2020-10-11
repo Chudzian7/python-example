7c46a4fb444f7831b16ce19d4c47fda6af1dc29a
Chudzian7 <55658520+Chudzian7@users.noreply.github.com>
2020-10-11 17:13:16 +0200

    Update test_hello.py

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -2,4 +2,16 @@ import hello
 
 
 def test_says_world():
-    assert hello.say_what() == 'worldsss'
+    assert hello.say_what() == 'world'
+    
+def test_says_swiat():
+    assert hello.say_what() == 'swiat'
+
+def test_1():
+    assert 'world' == 'world'
+
+def test_2():
+    assert 1 == 1
+
+def test_3():
+    assert '1' == '1'
